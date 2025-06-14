d451b3032fb6dca2aa5885ee8367bd7df6a36dbf
Pranav Bhat <pranavbhat2004@gmail.com>
2025-06-15 00:24:33 +0530

    edit basePath

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    basePath: "/portfolio-website",
+    basePath: "/",
     reactStrictMode: true,
     trailingSlash: true,
     // Remove output: "export" to enable dynamic routes and Sanity CMS
